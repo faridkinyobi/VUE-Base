a25eabe44486aeb32df2a16933c9a60d904469d8
faridkinyobi <faridkin7@gmail.com>
2025-03-27 20:34:23 +0700

    custom event and event validation

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,18 @@
 <template>
   <app-header />
   <div class="container">
-    <profile-page :name="name" :lastname="lastName" :userAge="userAge">
+    <profile-page
+      :name="name"
+      :lastname="lastName"
+      :userAge="userAge"
+      :hobby="hobby"
+      :userParent="userParent"
+      @update-cildren="userAge = $event"
+      @hallow="handleHallow"
+      :handleHobby="handleHobby"
+    >
     </profile-page>
-    <button @click="handleUpdate">Update</button>
+    <button @click="handleUpdate">Update Name</button>
   </div>
   <app-footer />
 </template>
@@ -22,12 +31,23 @@ export default {
     return {
       name: "Abdulah",
       lastName: "Abdul",
+      hobby: "swimming",
       userAge: 20,
+      userParent: {
+        father: "dhani",
+        mother: "siti",
+      },
     };
   },
   methods: {
     handleUpdate() {
-      this.name = 'alamsah';
+      this.name = "alamsah";
+    },
+    handleHallow() {
+      alert("Hallow word");
+    },
+    handleHobby(value) {
+      this.hobby = value;
     },
   },
 };
--- src/components/user/ProfilePage.vue
+++ src/components/user/ProfilePage.vue
@@ -4,14 +4,57 @@
     <ul>
       <li><span>Name</span>:{{ name }}</li>
       <li><span>lastName</span>: {{ lastname }}</li>
-      <li><span>lastName</span>: {{ userAge }}</li>
+      <li><span>userAge</span>: {{ userAge }}</li>
+      <li><span>Hobby</span>: {{ hobby }}</li>
+      <ul>
+        <li v-for="(key, value, index) in userParent" :key="index">
+          <span>{{ key }}</span
+          >:{{ value }}
+        </li>
+      </ul>
+      <button @click="handleUpdateCildren">Cildren Update age</button>
+      <button @click="HandleHallow">Hallow word</button>
+      <button @click="handleHobby(hobbys)">Hobby</button>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["name", "lastname","userAge"],
+  props: {
+    name: String,
+    lastname: String,
+    hobby: String,
+    userAge: Number,
+    userParent: Object,
+    handleHobby: Function,
+  },
+  data() {
+    return {
+      hobbys: "Fotball",
+    };
+  },
+  // cheking emits
+  // emits: ["update-cildren", "hallow"],
+  emits: {
+    "update-cildren": function (age) {
+      if (age === 21) {
+        return true;
+      } else {
+        alert("age must be less than 20");
+        return false;
+      }
+    },
+  },
+  // custom event
+  methods: {
+    handleUpdateCildren() {
+      this.$emit("update-cildren", 21);
+    },
+    HandleHallow() {
+      this.$emit("hallow");
+    },
+  },
 };
 </script>
 
